dee148f0a91e006020b15e16bfb33bd8cc8d6166
wupeng163 <wupenging163@163.com>
2022-01-05 17:37:19 +0800

    feat(layout/nav/index.vue)添加layOut组件

---
+++ src/layout/components/nav/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>导航条</div>
+</template>
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,18 +1,46 @@
 <template>
-  <div>
-    <router-view></router-view>
+  <div class="contenter">
+    <div class="header">
+      <NavBar />
+    </div>
+    <div class="main">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 
 <script lang="ts">
 import { defineComponent } from "vue";
-
+import NavBar from "@/layout/components/nav/index.vue";
 export default defineComponent({
+  components: { NavBar },
   setup() {
     return {};
   },
 });
 </script>
+
+<style lang="scss" scoped>
+.contenter {
+  width: 100%;
+  height: 100vh;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  .header {
+    height: 30px;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: red;
+  }
+  .main {
+    flex: 1;
+  }
+}
+</style>
 
 
